51be2625bd8a3ffd4a3541027d785d758f096b1f
Sanadix <114316682+Sanadix@users.noreply.github.com>
2024-10-07 19:03:17 +0200

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,35 +1,41 @@
-local library = {}
+local tweenService = game:GetService("TweenService")
 
-function library:notify(text, duration)
-    duration = duration or 3 
-    local screenGui = Instance.new("ScreenGui", game:GetService("CoreGui"))
-    local frame = Instance.new("Frame", screenGui)
-    frame.Size = UDim2.new(0, 300, 0, 50)
-    frame.Position = UDim2.new(1, -310, 1, -60)
-    frame.BackgroundColor3 = Color3.fromRGB(35, 35, 35)
-    frame.BorderColor3 = Color3.fromRGB(0, 0, 0)
-    frame.BorderSizePixel = 2
+local notifyLibrary = {
+    notifications = {},
+    playing = false
+}
 
-    local textLabel = Instance.new("TextLabel", frame)
+function notifyLibrary:notify(text, duration)
+    duration = duration or 3
+
+    local notification = Instance.new("Frame")
+    notification.Size = UDim2.new(0, 300, 0, 50)
+    notification.Position = UDim2.new(1, -320, 1, -100) 
+    notification.BackgroundColor3 = Color3.fromRGB(35, 35, 35)
+    notification.BorderSizePixel = 0
+    notification.BackgroundTransparency = 1
+    notification.Parent = game:GetService("CoreGui")
+
+    local textLabel = Instance.new("TextLabel", notification)
     textLabel.Size = UDim2.new(1, -10, 1, -10)
     textLabel.Position = UDim2.new(0, 5, 0, 5)
     textLabel.BackgroundTransparency = 1
     textLabel.Text = text
     textLabel.Font = Enum.Font.Code
     textLabel.TextSize = 14
-    textLabel.TextColor3 = Color3.fromRGB(244, 244, 244)
-    textLabel.TextStrokeTransparency = 0
-    textLabel.TextXAlignment = Enum.TextXAlignment.Center
-    textLabel.TextYAlignment = Enum.TextYAlignment.Center
+    textLabel.TextColor3 = Color3.fromRGB(240, 240, 240)
+    textLabel.TextWrapped = true
+    textLabel.TextXAlignment = Enum.TextXAlignment.Left
 
-    frame:TweenPosition(UDim2.new(1, -310, 1, -120), "Out", "Quad", 0.5, true)
+    tweenService:Create(notification, TweenInfo.new(0.3, Enum.EasingStyle.Quad, Enum.EasingDirection.Out), {BackgroundTransparency = 0}):Play()
+    table.insert(notifyLibrary.notifications, notification)
 
-    spawn(function()
-        wait(duration)
-        frame:TweenPosition(UDim2.new(1, -310, 1, 0), "In", "Quad", 0.5, true)
-        wait(0.5)
-        screenGui:Destroy()
+    task.delay(duration, function()
+        tweenService:Create(notification, TweenInfo.new(0.3, Enum.EasingStyle.Quad, Enum.EasingDirection.Out), {BackgroundTransparency = 1}):Play()
+        task.delay(0.3, function()
+            notification:Destroy()
+        end)
     end)
 end
 
-return library
+return notifyLibrary
