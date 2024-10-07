f38812ed5e1d48d0540ec7c528d1c5cd67e2580d
Sanadix <114316682+Sanadix@users.noreply.github.com>
2024-10-07 18:29:28 +0200

    Update first.lua

--- first.lua
+++ first.lua
@@ -1563,21 +1563,20 @@ function library:addTab(name)
             text.Parent = keybind
             text.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
             text.BackgroundTransparency = 1.000
-            text.Position = UDim2.new(0.02, 0, 0, 0) -- Ajustement de la position
-            text.Size = UDim2.new(0.7, 0, 1, 0) -- Ajustement de la taille
+            text.Position = UDim2.new(0.02, 0, 0, 0) 
+            text.Size = UDim2.new(0.7, 0, 1, 0)
             text.Font = Enum.Font.Code
             text.Text = args.text or args.flag
             text.TextColor3 = Color3.fromRGB(244, 244, 244)
             text.TextSize = 13.000
             text.TextStrokeTransparency = 0.000
             text.TextXAlignment = Enum.TextXAlignment.Left
-            
             button.Parent = keybind
             button.BackgroundColor3 = Color3.fromRGB(187, 131, 255)
             button.BackgroundTransparency = 1.000
             button.BorderSizePixel = 0
-            button.Position = UDim2.new(0.75, 0, 0, 0) -- Ajustement de la position du bouton
-            button.Size = UDim2.new(0.2, 0, 1, 0) -- Ajustement de la taille du bouton
+            button.Position = UDim2.new(0.75, 0, 0, 0) 
+            button.Size = UDim2.new(0.2, 0, 1, 0) 
             button.Font = Enum.Font.Code
             button.Text = "--"
             button.TextColor3 = Color3.fromRGB(155, 155, 155)
